50ea38380983e546e1913a33c00c899924ddc616
unknown <pinkats18@gmail.com>
2023-03-02 23:48:16 +0100

    edit script

--- src/display-interface.js
+++ src/display-interface.js
@@ -1,4 +1,5 @@
 import '../dist/style.css';
+import { getProjects } from './create-project';
 
 function storeProjectName(){
   let projectName = document.querySelector(".project-name-input").value;
@@ -6,27 +7,31 @@ function storeProjectName(){
 }
 
 function displayInterface () {
+
   const addProject = document.querySelector(".add-project");
   const addProjectPopup = document.querySelector(".add-project-name");
   const addProjectButtons = document.querySelectorAll(".add-project-popup-buttons");
   const addBtn = document.querySelector(".add-btn");
   const projectContainer = document.querySelector(".projects-container");
 
-  function projectTasksDisplay(){
-    
+  let projects = getProjects();
+
+  function createProject(){
+    let projectName = this.name;
+    const project = document.createElement('div');
+    project.textContent = projectName;
+    project.classList.add('project');
+    projectContainer.appendChild(project);
   }
 
+ 
   addProject.addEventListener("click", () => {addProject.classList.toggle("active");
                                             addProjectPopup.classList.toggle("active")});
   addProjectButtons.forEach(button => { button.addEventListener("click", () => { addProject.classList.toggle("active");
                                                                                 addProjectPopup.classList.toggle("active")});
   });
 
-  addBtn.addEventListener("click", () => {let projectName = storeProjectName();
-                                          const project = document.createElement('div');
-                                          project.textContent = projectName;
-                                          project.classList.add('project');
-                                          projectContainer.appendChild(project) });
+  addBtn.addEventListener("click", () => { projects.forEach(project => console.log(this.name)) });
 
 
 
